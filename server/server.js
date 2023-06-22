28b9bc08e29fd91f7afb792c98c1897bab482184
Sergio <spinfinite07@gmail.com>
2023-06-22 14:57:57 -0600

    message

--- server/server.js
+++ server/server.js
@@ -3,10 +3,16 @@ const cors = require('cors')
 const path = require('path')
 
 const app = express()
+
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static('${__dirname}/public'))
+app.get('/', (req,res) => {
 
+    res.status(200).sendFile(path.join(__dirname,"../public/index.html"))
+
+})
 
 app.listen(4000, () => {
 
